b17640d275ef84493fc155944461e19ae8d4189b
Lucas-Kamimura <lukkamimura@gmail.com>
2022-03-31 01:03:33 -0300

    cadastro em formulários

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -3,15 +3,32 @@ import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: ()=>void;
+  
 }
 
+
+
 export function NewTransactionModal({isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    console.log({
+      title,
+      value,
+      category,
+      type
+    })
+  }
 
   return (
     <Modal
@@ -27,20 +44,26 @@ export function NewTransactionModal({isOpen, onRequestClose }: NewTransactionMod
           <img src={closeImg} alt="Fechar modal" />
         </button>
 
-        <Container>
+        <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
         <input
            placeholder="Título"
+           value={title}
+           onChange={event => setTitle(event.target.value)}
         />
 
         <input
           type="number"
            placeholder="Valor"
+           value={value}
+           onChange={event => setValue(Number(event.target.value))}
         />
 
         <input
            placeholder="Categoria"
+           value={category}
+           onChange={event => setCategory(event.target.value)}
         />
 
         <TransactionTypeContainer>
@@ -48,6 +71,7 @@ export function NewTransactionModal({isOpen, onRequestClose }: NewTransactionMod
             type="button"
             onClick={() => {setType('deposit');}}
             isActive = {type === 'deposit'}
+            activeColor="green"
           >
             <img src={incomeImg} alt="Entrar" />
             <span>Entrada</span>
@@ -57,6 +81,7 @@ export function NewTransactionModal({isOpen, onRequestClose }: NewTransactionMod
             type="button"
             onClick={() => {setType('withdraw');}}
             isActive = {type === 'withdraw'}
+            activeColor="red"
           >
             <img src={outcomeImg} alt="Saída" />
             <span>Saída</span>
